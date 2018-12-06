c426be11a74ee75fae9c9ccf2a70a08c45321b41
lfrigodesouza <lfrigodesouza@outlook.com>
2018-12-05 23:38:16 -0200

    Criação do mecanismo de autenticação

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto pDto)
+        {
+            //validate request
+
+            pDto.Username = pDto.Username.ToLower();
+
+            if (await _repo.UserExists(pDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = pDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, pDto.Password);
+
+            return StatusCode(201, createdUser);
+        }
+    }
+}
