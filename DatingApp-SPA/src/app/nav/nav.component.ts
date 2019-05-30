2efde35bce685e0133956f8cc13853c0bd2fab5a
LFrigoDeSouza <lfrigodesouza@outlook.com>
2019-05-29 22:07:53 -0300

    Token Management and Login Improviments

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +10,30 @@ import { AuthService } from '../services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
-    }, error => {
-      console.log(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success('Logged in successfully');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-loggedIn() {
-  const token = localStorage.getItem('token');
-  return !!token;
-}
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
 
-logout() {
-  localStorage.removeItem('token');
-  console.log('logged out');
-}
-
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('Logged out');
+  }
 }
